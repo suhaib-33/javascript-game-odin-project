4192796b83755bbe519c27b94c0dfec8fa6ede6b
suhaib <suhaib.build@gmail.com>
2026-04-08 23:12:21 +0500

    changed font style

--- javascript.js
+++ javascript.js
@@ -1,79 +1,46 @@
 
-
-
-/*         let humanScore = 0;
+    const choices = ["rock", "paper", "scissor"];
+     const computerDisplay = document.getElementById ("computerDisplay");
+     const playerDisplay = document.getElementById ("playerDisplay");
+     const resultDisplay = document.getElementById ("resultDisplay");
+     const computerScoreDisplay = document.getElementById ("computerScoreDisplay");
+     const playerScoreDisplay = document.getElementById ("playerScoreDisplay");
+        let playerScore = 0;
         let computerScore = 0;
 
+     function playGame (playerChoice) {
 
-        function getComputerChoice () {
-            let choice = ["rock", "paper", "scissor"];
-            return choice[Math.floor(Math.random () * choice.length)]
-        };
+        const computerChoice = choices[Math.floor(Math.random () * 3)];
+        let result = ""
 
-        function getHumanChoice () {
-            let response = prompt ("Enter: rock, paper, scissor");
-
-            if (!response) return getHumanChoice();
-
-            response= response.toLowerCase();
-
-            if (response === "rock" || response === "paper" || response === "scissor") {
-                return response
-            } else {
-                return getHumanChoice();
-            }
-        };
-
-        function playRound (humanChoice, computerChoice) {
-
-         if (humanChoice === computerChoice) {
-    console.log("it is a tie");
-
- } else if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
-    console.log("You lose! Paper beats Rock");
- } else if (humanChoice === "rock" && computerChoice === "scissor") {
-    humanScore++;
-    console.log("You win! Rock beats scissor");
- } else if (humanChoice === "paper" && computerChoice === "scissor") {
-    computerScore++;
-    console.log("You lose! Scissor beats Paper");
- } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanScore++;
-    console.log("You win! paper beats rock");
- } else if (humanChoice === "scissor" && computerChoice === "paper") {
-    humanScore++;
-    console.log("You win! Scissor beats Paper");
- } else if (humanChoice === "scissor" && computerChoice === "rock") {
-    computerScore++;
-    console.log("You lose! Rock beats scissor");
- }
-        };
-
-
-        function playGame () {
-            for (let i=1; i <= 5; i++) {
-                console.log (`----ROUND ${i}----`);
-                let humanChoice = getHumanChoice();
-                let computerChoice = getComputerChoice();
-                playRound (humanChoice, computerChoice);
-            }
-
-        console.log (`YOUR SCORE = ${humanScore} , COOMPUTER SCORE = ${computerScore}`)
-
-        if (humanScore > computerScore) {
-            console.log ("Congrats!!! you win the match");
-        } else if (humanScore < computerScore) {
-            console.log ("Better Try next time. YOU Losee");
+        if (computerChoice === playerChoice) {
+            result = "it is a tie"
         } else {
-            console.log ("This match is tie, Play again!!");
+            switch (playerChoice) {
+                case "rock":
+                    result = (computerChoice === "scissor") ? "YOU WIN" : "YOU LOSE"
+                    break;
+                case "paper":
+                    result = (computerChoice === "rock") ? "YOU WIN" : "YOU LOSE"
+                    break;
+                case "scissor":
+                    result = (computerChoice === "paper") ? "YOU WIN" : "YOU LOSE"
+                    break;
+            }
         }
-    };
 
-     playGame(); */
+        playerDisplay.textContent = `PLAYER ${playerChoice}`;
+        computerDisplay.textContent = `COMPUTER ${computerChoice}`;
+        resultDisplay.textContent = result;
 
-     const btn = document.querySelector("#btn");
-
-     btn.addEventListener ("click", (e) => {
-        alert("Button clicked Atttention!");
-     });
+        switch (result) {
+            case "YOU WIN":
+            playerScore++
+            playerScoreDisplay.textContent = playerScore;
+            break;
+            case "YOU LOSE":
+            computerScore++
+            computerScoreDisplay.textContent = computerScore;
+            break;
+        }
+     };
